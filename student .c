9184106586137286d2172c1a0cae42bdc190459f
Rahin4 <137500367+Rahin4@users.noreply.github.com>
2025-10-30 20:52:18 +0600

    Add files via upload

---
+++ student .c
@@ -0,0 +1,38 @@
+//input marks of 5 subjects and calculate total, average, and grade.
+
+#include <stdio.h>
+
+int ban, eng, eco, math, cal,average,total;
+float grade;
+
+void calcu(){
+    total= ban+eng+eco+math+cal;
+    average=total/5;
+    int gradeavg= (int)average/10;
+    printf("%d is total and %d is average\n and you have got a ",total, average);
+    switch(gradeavg){
+    case 10:
+    case 9:
+    case 8:
+        printf("A+");
+        break;
+        break;
+    case 7:
+        printf("A");
+        break;
+    case 6:
+        printf("B");
+        break;
+    default:
+        printf("Fail");
+    }
+}
+
+int main(){
+    printf("enter your marks :: ban; eng ; eco ; math ; cal; separately : ");
+    scanf("%d %d %d %d %d", &ban, &eng, &eco, &math, &cal);
+    calcu();
+    return 0;
+}
+
+
